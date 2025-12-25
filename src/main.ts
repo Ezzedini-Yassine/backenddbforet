8f963caef6e58bd018bd561fe56104e4efadd209
yassine <yacine.ezdini@gmail.com>
2025-12-25 01:25:02 +0100

    Allow different origin calls in the frontend for local dev + type casting via transform +  DTO filtering via whitelist + block frontend request from going through if there is an extra field compared to the DTO

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  //allowing this in development only (good for testing), when moving to production, make sure to disable CORS or limit it to specific origins
+  const appOptions = { cors: true };
+  const app = await NestFactory.create(AppModule, appOptions);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   setupSwagger(app);
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
